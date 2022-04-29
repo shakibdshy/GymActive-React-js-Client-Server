266ab52d7b3cc41ef7a079066c053be7a6c8a36d
shakibdshy <shakibdshy@gmail.com>
2022-04-30 01:28:48 +0600

    Header and Footer

--- src/Pages/Banner/Banner.js
+++ src/Pages/Banner/Banner.js
@@ -3,19 +3,16 @@ import React from "react";
 const Banner = () => {
   return (
     <main className='mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28'>
-      <div className='sm:text-center lg:text-left'>
+      <div className='sm:text-center'>
         <h1 className='text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl'>
-          <span className='block xl:inline'>Data to enrich your</span>{" "}
-          <span className='block text-indigo-600 xl:inline'>
-            online business
-          </span>
+          <span className='block xl:inline'>Train How</span>{" "}
+          <span className='block text-indigo-600 xl:inline'>You Want</span>
         </h1>
-        <p className='mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0'>
-          Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem
-          cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat
-          aliqua.
+        <p className='mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-3xl sm:mx-auto md:mt-8 md:text-xl'>
+          It is a long established fact that a reader will be distracted by the
+          readable content of a page when looking at its layout.
         </p>
-        <div className='mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start'>
+        <div className='mt-5 sm:mt-8 sm:flex sm:justify-center gap-4'>
           <div className='rounded-md shadow'>
             <a
               href='#'
@@ -23,7 +20,7 @@ const Banner = () => {
               Get started
             </a>
           </div>
-          <div className='mt-3 sm:mt-0 sm:ml-3'>
+          <div>
             <a
               href='#'
               className='w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10'>
@@ -31,13 +28,6 @@ const Banner = () => {
             </a>
           </div>
         </div>
-      </div>
-      <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-        <img
-          className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
-          src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"
-          alt=""
-        />
       </div>
     </main>
   );
