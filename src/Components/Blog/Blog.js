5ec203e87059512c9cb43ce0752795ce95d00f6b
Shakib D shy <shakibdshy@gmail.com>
2022-05-02 15:49:34 +0600

    Section Title Update

--- src/Components/Blog/Blog.js
+++ src/Components/Blog/Blog.js
@@ -28,6 +28,7 @@ const Blog = () => {
   return (
     <section className='pt-20 lg:pt-[120px] pb-10 lg:pb-20 bg-[#F3F4F6]'>
       <div className='mb-16 container mx-auto text-center'>
+        <span className="mb-2 font-bold text-xl text-center text-red-500 block uppercase">Blog</span>
         <h2 className='text-4xl text-black uppercase font-bold'>
           LATEST <span className='text-red-600'>News</span> & ARTICLES
         </h2>
